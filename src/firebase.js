25f661e604e89924f0b3a5db7ff362a2c078870b
Abirath <abirathamir@gmail.com>
2020-10-16 07:59:58 +1300

    Initial Check-In

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCOcdPIVA4p1-cyuOrt_fQPTps39RPEt4s",
+    authDomain: "clone-c8238.firebaseapp.com",
+    databaseURL: "https://clone-c8238.firebaseio.com",
+    projectId: "clone-c8238",
+    storageBucket: "clone-c8238.appspot.com",
+    messagingSenderId: "856137969196",
+    appId: "1:856137969196:web:f85598a8ce7ac93e286b89",
+    measurementId: "G-TPK21JXDVP"
+  };
